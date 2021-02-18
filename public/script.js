abce2edee754637850d96cd6cc48c80adc4c693e
long ho <long_ho@datahouse.com>
2021-02-18 20:39:57 +0700

    Finish play and stop video - Long Ho - 18.02.2021

--- public/script.js
+++ public/script.js
@@ -107,22 +107,48 @@ const muteUnmute = () => {
     setMuteButton();
     myVideoStream.getAudioTracks()[0].enabled = true;
   }
-  console.log(myVideoStream.getAudioTracks()[0].enabled);
-};
-
-const setUnmuteButton = () => {
-  const html = `
-    <i class="unmute fa fa-microphone-slash"></i>
-    <span>Unmute</span>
-  `;
-  document.querySelector(".main__mute_button").innerHTML = html;
 }
 
 const setMuteButton = () => {
   const html = `
     <i class="fa fa-microphone"></i>
     <span>Mute</span>
-  `;
-  document.querySelector(".main__mute_button").innerHTML = html;
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fa fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fa fa-video-camera"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+    <i class="unmute fa fa-pause-circle"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
 }
 
